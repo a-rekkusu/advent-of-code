70ae7062c0df28a245ab7c32300c0cae6b3d5c85
a-rekkusu <fischer.a@mailbox.org>
2024-12-02 23:16:09 +0100

    feat: solve 2024.02 part 1

--- src/2024/02/part1.ts
+++ src/2024/02/part1.ts
@@ -2,12 +2,11 @@ export function executePart1(input: string[]): number {
     const records = input.map((record) => record.split(' '))
 
     let safeCount = 0
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of
-    for (let i = 0; i < records.length; i++) {
+    for (const record of records) {
         let isRecordSafe = true
         let isAscending = null
-        for (let j = 1; j < records[i].length - 1; j++) {
-            const safeResult = isSafeLevel(+records[i][j - 1], +records[i][j], isAscending)
+        for (let j = 1; j < record.length; j++) {
+            const safeResult = isSafeLevel(+record[j - 1], +record[j], isAscending)
             if (!safeResult.isSafe) { 
                 isRecordSafe = false 
                 break
@@ -16,9 +15,8 @@ export function executePart1(input: string[]): number {
         }
         if (isRecordSafe) ++safeCount
     }
-    console.log(safeCount)
 
-    return 0
+    return safeCount
 }
 
 function isSafeLevel(previous: number, current: number, isAscending: boolean | null) : { isAscending: boolean | null; isSafe: boolean}  {
