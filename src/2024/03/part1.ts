aeec989c968e24abda9e94d986e904de978afebd
a-rekkusu <fischer.a@mailbox.org>
2024-12-05 19:33:28 +0100

    refactor: clean up solution

--- src/2024/03/part1.ts
+++ src/2024/03/part1.ts
@@ -4,14 +4,14 @@ import path from 'path'
 
 const puzzle = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8')
 
-function multiply(input: string): number {
-    const [left, right] = input.split(',')
-    return +left.substring(4) * +right.substring(0, right.length - 1) 
-}
-
 function solve(input: string): number {
     const matches = [...input.matchAll(/mul\(\d{1,3},\d{1,3}\)/g)]
     return matches.reduce((acc, matches) => acc + multiply(matches[0]), 0)
+}
+
+function multiply(input: string): number {
+    const [left, right] = input.split(',')
+    return +left.substring(4) * +right.substring(0, right.length - 1) 
 }
 
 assert.equal(solve(puzzle), 161)
--- src/2024/03/part2.ts
+++ src/2024/03/part2.ts
@@ -4,32 +4,30 @@ import path from 'path'
 
 const puzzle = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8')
 
-function multiply(input: string): number {
-    const [left, right] = input.split(',')
-    return +left.substring(4) * +right.substring(0, right.length - 1) 
-}
-
 function solve(input: string): number {
     const dos = input.split('do()')
     const toMatch: string[] = []
 
-    for (const doMult of dos) {
-        const doBeforeDont = ''
+    dos.forEach((doMult) => {
         let filteredDo = doMult
         if (doMult.includes('don\'t()')) {
-            console.log(doMult.split('don\'t()'))
             doMult.split('don\'t()').forEach((dont, i) => { 
-                if (i === 0) { toMatch.push(doBeforeDont) } else {
+                if (i !== 0) {
                     filteredDo = filteredDo.replace(dont, '') 
                 }
             })
         }
-        
-        toMatch.push(filteredDo)
-    }
 
+        toMatch.push(filteredDo)
+    })
+    
     const matches = [...toMatch.join('').matchAll(/mul\(\d{1,3},\d{1,3}\)/g)]
     return matches.reduce((acc, matches) => acc + multiply(matches[0]), 0)
+}
+
+function multiply(input: string): number {
+    const [left, right] = input.split(',')
+    return +left.substring(4) * +right.substring(0, right.length - 1) 
 }
 
 assert.equal(solve(puzzle), 48)
