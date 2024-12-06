b6d6d3dffd8e44fc53717befe7a6b30df7412943
a-rekkusu <fischer.a@mailbox.org>
2024-12-06 18:04:09 +0100

    feat: add thoughts for day 4

---
+++ src/2024/04/part1.ts
@@ -0,0 +1,10 @@
+// for each line
+// for each X
+// find all XMAS(currentLine, 3 previousLines, 3 nextLines)
+// -> try find forwards
+// -> try find backwards
+// -> try find top (straight, diagonally left, right)
+// -> try find bottom (straight, diagonally left, right)
+// -> increase count for all branches
+
+// maybe use Graph algorithm, DFS or BFS
