0c3d3010972ab01eda62d50ca6ed14684f432df0
a-rekkusu <fischer.a@mailbox.org>
2025-12-05 00:27:50 +0100

    chore: reduce repository structure

--- tests/2023/day1/day1.test.ts
+++
@@ -1,6 +0,0 @@
-import { calculateSum } from '../../../src/2023/day1'
-import { sampleData  } from './testData'
-
-it('sample data test', () => {
-  expect(calculateSum(sampleData)).toEqual(281)
-})
