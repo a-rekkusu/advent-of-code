a876e7544671747297f2b9fc2ebf639b8a6d36fa
a-rekkusu <fischer.a@mailbox.org>
2025-12-04 10:17:22 +0100

    feat: add runtime metric to 2025.02

--- src/2025/02/part1.ts
+++ src/2025/02/part1.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 
+const startTime = performance.now()
+
 const puzzle = fs
   .readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8')
   .split(',')
@@ -32,4 +34,6 @@ ranges.forEach((range) => {
   }
 })
 
+const endTime = performance.now()
+console.log(`runtime: ${endTime - startTime} ms`)
 console.log(result)
